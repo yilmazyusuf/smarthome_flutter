d7a0e6cc94b6ad1552d52bba29766915167296f5
Yusuf <y.yilmaz@hotmail.com.tr>
2020-12-14 23:52:14 +0300

    sound

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,25 +1,13 @@
 import 'dart:async';
 import 'dart:convert';
-import 'dart:typed_data';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_sound/public/util/wave_header.dart';
 import 'package:qubisch_home/api/humidity/LivingRoom.dart';
-import 'package:sound_stream/sound_stream.dart';
-import 'dart:developer' as developer;
-import 'package:flutter/foundation.dart';
-import 'package:web_socket_channel/io.dart';
-import 'package:web_socket_channel/web_socket_channel.dart';
 import 'dart:io';
-import 'dart:async';
 import 'package:permission_handler/permission_handler.dart';
-import 'package:flutter/foundation.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:flutter_sound/flutter_sound.dart';
-import 'package:http/http.dart' as http;
-import 'package:path/path.dart';
-import 'package:async/async.dart';
 import 'package:dio/dio.dart';
 
 typedef _Fn = void Function();
@@ -30,14 +18,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeState extends State<HomeScreen> {
-  RecorderStream _recorder = RecorderStream();
-  PlayerStream _player = PlayerStream();
-  List<Uint8List> _micChunks = [];
 
-  bool _isPlaying = false;
-  StreamSubscription _recorderStatus;
-  StreamSubscription _playerStatus;
-  StreamSubscription _audioStream;
 
   FlutterSoundPlayer _mPlayer = FlutterSoundPlayer();
   FlutterSoundRecorder _mRecorder = FlutterSoundRecorder();
@@ -106,9 +87,6 @@ class _HomeState extends State<HomeScreen> {
 
   @override
   void dispose() {
-    _recorderStatus?.cancel();
-    _playerStatus?.cancel();
-    _audioStream?.cancel();
 
     _mPlayer.closeAudioSession();
     _mPlayer = null;
@@ -169,22 +147,15 @@ class _HomeState extends State<HomeScreen> {
   }
 
   void sendToApi() async {
-    var tempDirD = await getTemporaryDirectory();
-    var path = '${tempDirD.path}/flutter_sound_tmp.wav';
-    var filOut = File(path);
-    Directory directory = await getApplicationDocumentsDirectory();
+    Directory directory = await getExternalStorageDirectory();
+    String pathi = directory.path;
+    var filOut = File('$pathi/filename.wav');
+    final file = MultipartFile.fromBytes(filOut.readAsBytesSync(), filename: 'filename.wav');
+
+    FormData formData = FormData.fromMap({"file": file});
+    await dio.post("http://85.100.127.47:5000/stream", data: formData);
 
 
-    String toCopy = '${directory.path}/filename.wav';
-    File newImage = await filOut.copy('${directory.path}/filename.wav').whenComplete(() async {
-
-      print('passs');
-      print(toCopy);
-      //final file = MultipartFile.fromBytes(File(toCopy).readAsBytesSync(), filename: 'filename.wav');
-
-      //FormData formData = FormData.fromMap({"file": file});
-      //await dio.post("http://85.100.127.47:5000/stream", data: formData);
-    });
   }
 
   _Fn getRecorderFn() {
@@ -252,58 +223,49 @@ class _HomeState extends State<HomeScreen> {
                       children: [
                         Text(
                           'Sıcaklık',
+                            style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 2.0)
                         ),
                         Text(
-                          snapshot.data.temperature + ' *C',
+                          snapshot.data.temperature + '℃',
                           style: TextStyle(
                             fontWeight: FontWeight.bold,
+                            fontSize: 30
+
+
                           ),
                         )
                       ],
                     ),
                     Row(
+
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
                       children: [
-                        Text('Nem'),
+
+                        Text(
+                            'Nem',
+                            style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 2.0)
+
+                        ),
                         Text(
                           snapshot.data.humidity + '%',
                           style: TextStyle(
                             fontWeight: FontWeight.bold,
+                              fontSize: 30
                           ),
                         )
                       ],
                     ),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    ),
-                    Container(
-                      margin: const EdgeInsets.all(3),
-                      padding: const EdgeInsets.all(3),
-                      height: 80,
-                      width: double.infinity,
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                        color: Color(0xFFFAF0E6),
-                        border: Border.all(
-                          color: Colors.indigo,
-                          width: 3,
-                        ),
-                      ),
-                      child: Row(children: [
-                        RaisedButton(
+                      children: [
+                        IconButton(
+                          padding: EdgeInsets.only(top: 50.0),
+                          iconSize: 150,
+                          icon: Icon(_mRecorder.isRecording  ? Icons.mic_off : Icons.mic),
                           onPressed: getRecorderFn(),
-                          color: Colors.white,
-                          disabledColor: Colors.grey,
-                          child:
-                              Text(_mRecorder.isRecording ? 'Stop' : 'Record'),
                         ),
-                        SizedBox(
-                          width: 20,
-                        ),
-                        Text(_mRecorder.isRecording
-                            ? 'Recording in progress'
-                            : 'Recorder is stopped'),
-                      ]),
+                      ],
                     ),
                   ],
                 );
