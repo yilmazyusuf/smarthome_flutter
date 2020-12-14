d46fd121ffe527f68e51ca7f3e41256c50999142
Yusuf <y.yilmaz@hotmail.com.tr>
2020-12-14 04:52:32 +0300

    a

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
+import 'dart:convert';
 import 'dart:typed_data';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_sound/public/util/wave_header.dart';
 import 'package:qubisch_home/api/humidity/LivingRoom.dart';
 import 'package:sound_stream/sound_stream.dart';
 import 'dart:developer' as developer;
@@ -15,6 +17,10 @@ import 'package:permission_handler/permission_handler.dart';
 import 'package:flutter/foundation.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:flutter_sound/flutter_sound.dart';
+import 'package:http/http.dart' as http;
+import 'package:path/path.dart';
+import 'package:async/async.dart';
+import 'package:dio/dio.dart';
 
 typedef _Fn = void Function();
 
@@ -27,7 +33,7 @@ class _HomeState extends State<HomeScreen> {
   RecorderStream _recorder = RecorderStream();
   PlayerStream _player = PlayerStream();
   List<Uint8List> _micChunks = [];
-  bool _isRecording = false;
+
   bool _isPlaying = false;
   StreamSubscription _recorderStatus;
   StreamSubscription _playerStatus;
@@ -39,11 +45,44 @@ class _HomeState extends State<HomeScreen> {
   bool _mRecorderIsInited = false;
   bool _mplaybackReady = false;
   String _mPath;
+  Dio dio = new Dio();
 
   final LivingRoom livingRoom = LivingRoom();
 
   Socket socket;
   List<int> recordedData = [];
+
+  Future<void> pcmToWaveXXXX({
+    /// Stereophony is not yet implemented
+    int numChannels = 1,
+    int sampleRate = 16000,
+  }) async {
+    Directory directory = await getExternalStorageDirectory();
+    String pathi = directory.path;
+    Directory tempDir = await getExternalStorageDirectory();
+    String inp = '${tempDir.path}/myFile.pcm';
+
+    var filOut = File('$pathi/filename.wav');
+    var filIn = File(inp);
+
+    var size = filIn.lengthSync();
+    Log.i(
+        'pcmToWave() : input = $inp,  output = $pathi/filename.wav,  size = $size');
+    var sink = filOut.openWrite();
+
+    var header = WaveHeader(
+      WaveHeader.formatPCM,
+      numChannels = numChannels, //
+      sampleRate = sampleRate,
+      16, // 16 bits per byte
+      size, // total number of bytes
+    );
+    header.write(sink);
+    await filIn.open();
+    var buffer = filIn.readAsBytesSync();
+    sink.add(buffer.toList());
+    await sink.close();
+  }
 
   void socketSet(Socket _socket) {
     socket = _socket;
@@ -63,9 +102,6 @@ class _HomeState extends State<HomeScreen> {
     });
 
     super.initState();
-    //initPlugin();
-    //requestWritePermission();
-    //initSocket();
   }
 
   @override
@@ -74,7 +110,6 @@ class _HomeState extends State<HomeScreen> {
     _playerStatus?.cancel();
     _audioStream?.cancel();
 
-    stopPlayer();
     _mPlayer.closeAudioSession();
     _mPlayer = null;
 
@@ -92,8 +127,10 @@ class _HomeState extends State<HomeScreen> {
   }
 
   Future<void> openTheRecorder() async {
-    var status = await Permission.microphone.request();
-    if (status != PermissionStatus.granted) {
+    var statusMic = await Permission.microphone.request();
+    var statusStorage = await Permission.storage.request();
+    if (statusMic != PermissionStatus.granted ||
+        statusStorage != PermissionStatus.granted) {
       throw RecordingPermissionException('Microphone permission not granted');
     }
 
@@ -122,33 +159,32 @@ class _HomeState extends State<HomeScreen> {
   }
 
   Future<void> stopRecorder() async {
-    await _mRecorder.stopRecorder();
+    await _mRecorder.stopRecorder().whenComplete(() {
+      Future cont = pcmToWaveXXXX();
+      cont.whenComplete(() {
+        sendToApi();
+      });
+    });
     _mplaybackReady = true;
   }
 
-  void play() async {
-    assert(_mPlayerIsInited &&
-        _mplaybackReady &&
-        _mRecorder.isStopped &&
-        _mPlayer.isStopped);
+  void sendToApi() async {
+    var tempDirD = await getTemporaryDirectory();
+    var path = '${tempDirD.path}/flutter_sound_tmp.wav';
+    var filOut = File(path);
+    Directory directory = await getApplicationDocumentsDirectory();
 
-    Directory tempDir = await getExternalStorageDirectory();
-    String outputFile = '${tempDir.path}/myFile.pcm';
-    print("dosya:"+outputFile);
 
-    await _mPlayer.startPlayer(
-        fromURI: outputFile,
-        codec: Codec.pcm16,
-        numChannels: 1,
-        sampleRate:16000,
-        whenFinished: () {
-          setState(() {});
-        });
-    setState(() {});
-  }
+    String toCopy = '${directory.path}/filename.wav';
+    File newImage = await filOut.copy('${directory.path}/filename.wav').whenComplete(() async {
 
-  Future<void> stopPlayer() async {
-    await _mPlayer.stopPlayer();
+      print('passs');
+      print(toCopy);
+      //final file = MultipartFile.fromBytes(File(toCopy).readAsBytesSync(), filename: 'filename.wav');
+
+      //FormData formData = FormData.fromMap({"file": file});
+      //await dio.post("http://85.100.127.47:5000/stream", data: formData);
+    });
   }
 
   _Fn getRecorderFn() {
@@ -161,158 +197,6 @@ class _HomeState extends State<HomeScreen> {
             stopRecorder().then((value) => setState(() {}));
           };
   }
-
-  _Fn getPlaybackFn() {
-    if (!_mPlayerIsInited || !_mplaybackReady || !_mRecorder.isStopped) {
-      return null;
-    }
-    return _mPlayer.isStopped
-        ? play
-        : () {
-            stopPlayer().then((value) => setState(() {}));
-          };
-  }
-
-  requestWritePermission() async {
-    var status = await Permission.storage.status;
-  }
-
-  Future<String> get _localPath async {
-    final directory = await getApplicationDocumentsDirectory();
-
-    return directory.path;
-  }
-
-  void initSocket() async {
-    await Socket.connect("192.168.1.8", 48748).then((Socket sock) {
-      socket = sock;
-      socket.listen(dataHandler,
-          onError: errorHandler, onDone: doneHandler, cancelOnError: false);
-      socketSet(socket);
-    }).catchError((AsyncError e) {
-      print("Unable to connect: $e");
-    });
-
-    //Connect standard in to the socket
-    stdin.listen(
-        (data) => socket.write(new String.fromCharCodes(data).trim() + '\n'));
-  }
-
-  void dataHandler(data) {
-    print(new String.fromCharCodes(data).trim());
-  }
-
-  void errorHandler(error, StackTrace trace) {
-    print(error);
-  }
-
-  void doneHandler() {
-    socket.destroy();
-  }
-
-  Future<void> initPlugin() async {
-    _recorderStatus = _recorder.status.listen((status) {
-      if (mounted)
-        setState(() {
-          _isRecording = status == SoundStreamStatus.Playing;
-        });
-    });
-
-    _audioStream = _recorder.audioStream.listen((data) {
-      if (_isPlaying) {
-        _player.writeChunk(data);
-      } else {
-        socket.add(data);
-
-        //recordedData.addAll(data);
-        _micChunks.add(data);
-      }
-    });
-
-    _playerStatus = _player.status.listen((status) {
-      if (mounted)
-        setState(() {
-          _isPlaying = status == SoundStreamStatus.Playing;
-        });
-    });
-
-    await Future.wait([
-      _recorder.initialize(),
-      _player.initialize(),
-    ]);
-  }
-
-  void _play() async {
-    await _player.start();
-
-    if (_micChunks.isNotEmpty) {
-      for (var chunk in _micChunks) {
-        await _player.writeChunk(chunk);
-      }
-      //print('dosya');
-      //await save(recordedData, 44100);
-
-      _micChunks.clear();
-    }
-  }
-
-  Future<void> save(List<int> data, int sampleRate) async {
-    final path = await _localPath;
-    File recordedFile = File('$path/recordedFile.wav');
-    print(recordedFile);
-
-    var channels = 1;
-
-    int byteRate = ((16 * sampleRate * channels) / 8).round();
-
-    var size = data.length;
-
-    var fileSize = size + 36;
-
-    Uint8List header = Uint8List.fromList([
-      // "RIFF"
-      82, 73, 70, 70,
-      fileSize & 0xff,
-      (fileSize >> 8) & 0xff,
-      (fileSize >> 16) & 0xff,
-      (fileSize >> 24) & 0xff,
-      // WAVE
-      87, 65, 86, 69,
-      // fmt
-      102, 109, 116, 32,
-      // fmt chunk size 16
-      16, 0, 0, 0,
-      // Type of format
-      1, 0,
-      // One channel
-      channels, 0,
-      // Sample rate
-      sampleRate & 0xff,
-      (sampleRate >> 8) & 0xff,
-      (sampleRate >> 16) & 0xff,
-      (sampleRate >> 24) & 0xff,
-      // Byte rate
-      byteRate & 0xff,
-      (byteRate >> 8) & 0xff,
-      (byteRate >> 16) & 0xff,
-      (byteRate >> 24) & 0xff,
-      // Uhm
-      ((16 * channels) / 8).round(), 0,
-      // bitsize
-      16, 0,
-      // "data"
-      100, 97, 116, 97,
-      size & 0xff,
-      (size >> 8) & 0xff,
-      (size >> 16) & 0xff,
-      (size >> 24) & 0xff,
-      ...data
-    ]);
-
-    return recordedFile.writeAsBytes(header, flush: true);
-  }
-
-  TextEditingController _controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -391,20 +275,6 @@ class _HomeState extends State<HomeScreen> {
                     ),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        IconButton(
-                          iconSize: 96.0,
-                          icon: Icon(_isRecording ? Icons.mic_off : Icons.mic),
-                          onPressed:
-                              _isRecording ? _recorder.stop : _recorder.start,
-                        ),
-                        IconButton(
-                          iconSize: 96.0,
-                          icon:
-                              Icon(_isPlaying ? Icons.pause : Icons.play_arrow),
-                          onPressed: _isPlaying ? _player.stop : _play,
-                        ),
-                      ],
                     ),
                     Container(
                       margin: const EdgeInsets.all(3),
@@ -435,34 +305,6 @@ class _HomeState extends State<HomeScreen> {
                             : 'Recorder is stopped'),
                       ]),
                     ),
-                    Container(
-                      margin: const EdgeInsets.all(3),
-                      padding: const EdgeInsets.all(3),
-                      height: 80,
-                      width: double.infinity,
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                        color: Color(0xFFFAF0E6),
-                        border: Border.all(
-                          color: Colors.indigo,
-                          width: 3,
-                        ),
-                      ),
-                      child: Row(children: [
-                        RaisedButton(
-                          onPressed: getPlaybackFn(),
-                          color: Colors.white,
-                          disabledColor: Colors.grey,
-                          child: Text(_mPlayer.isPlaying ? 'Stop' : 'Play'),
-                        ),
-                        SizedBox(
-                          width: 20,
-                        ),
-                        Text(_mPlayer.isPlaying
-                            ? 'Playback in progress'
-                            : 'Player is stopped'),
-                      ]),
-                    )
                   ],
                 );
               } else if (snapshot.hasError) {
